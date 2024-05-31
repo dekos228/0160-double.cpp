b0e355684828b833d65ed8d1f1346e0d8c2f325e
dekos228 <dekos589191@gmail.com>
2024-05-31 20:30:36 +0200

    Create 0160-double.cpp

---
+++ 0160-double.cpp
@@ -0,0 +1,19 @@
+﻿// double.cpp
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+  using namespace std;
+
+  double a = 0, b = 0; // Числа с плавающей запятой.
+  cout << "a = ";
+  cin >> a;
+  cout << "b = ";
+  cin >> b;
+
+  cout << "quotient a:b  = " << a / b << endl;
+  //cout << "remainder a:b = " << a % b << endl;
+  // Операция взятия остатка не определена.
+  return EXIT_SUCCESS;
+}
